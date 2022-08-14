b0c28633e499f6f07f6ce7673aabd648abba26f9
TREVTAL <trevtal@gmail.com>
2022-08-14 15:08:19 -0500

    more tweaks to info moved into readme and started troubleshooting

---
+++ README.md
@@ -0,0 +1,27 @@
+
+#Jorge
+
+
+el mero mero
+
+
+alksuf
+
+lkjasbdv
+
+lkajbsf;lkjhabf
+
+lakhbf
+
+3  The Unlicense 
+ 	License with no conditions, only limitations to author
+
+trevtal
+
+treval some email
+
+https://img.shields.io/badge/License-3-yellowgreen
+
+https://choosealicense.com/licenses/unlicense/
+
+
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
 import fs from 'fs';
+let badgeColor;
+let licenseURL;
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -14,9 +16,28 @@ const questions = [
     "Please enter your GitHub User",
     "Please enter you email address"
 ];
+let badge;
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+const generateReadMe = ({name, description, installation, usage, credit, test, license, github, email}) =>
+`
+#${name}\n
+
+${description}\n
+
+${installation}\n
+${usage}\n
+${credit}\n
+${test}\n
+${license}\n
+${github}\n
+${email}\n
+${badge}\n
+${licenseURL}\n
+
+`;
+
 
 // TODO: Create a function to initialize app
 // function init() {}
@@ -54,9 +75,9 @@ inquirer.prompt([
       {
         type: 'list',
         message:     "\n\nPlease select the type of license your working with",
-        choices:    ["GNU GPLv2 \n \tPermisive license, let's do anything except distributing closed source versions" , 
-                    "MIT License \n \tShort and to the point, lets people do almost anything they want with your project, only requires preservation of copyright and license" , 
-                    "The Unlicense \n \tLicense with no conditions, only limitations to author"],
+        choices:    ["1  GNU GPLv2 \n \tPermisive license, let's do anything except distributing closed source versions" , 
+                    "2  MIT License \n \tShort and to the point, lets people do almost anything they want with your project, only requires preservation of copyright and license" , 
+                    "3  The Unlicense \n \tLicense with no conditions, only limitations to author"],
         name: 'license',
       },
       {
@@ -69,7 +90,28 @@ inquirer.prompt([
         message:  "\n\nPlease enter you email address",
         name: 'email',
       },
-])
+]).then((response) => {
+    let licenseOption = response.license.charAt(0);
+    console.log(response.license);
+    switch (licenseOption){
+        case '1':
+            badgeColor='brightgreen';  
+            licenseURL ='https://choosealicense.com/licenses/gpl-3.0/';
+            break;
+        case '2':
+            badgeColor = 'green';
+            licenseURL ='https://choosealicense.com/licenses/mit/';
+            break;
+        case '3':
+            badgeColor = 'yellowgreen';
+            licenseURL ='https://choosealicense.com/licenses/unlicense/';
+            break;
+    };
+    badge = `https://img.shields.io/badge/License-${licenseOption}-${badgeColor}`;
+    const readMeFinal = generateReadMe(response, badge, licenseURL);
+    fs.writeFile('README.md', readMeFinal, (err) =>
+    err ? console.log(err) : console.log('Success, README created')  )
+});
 
 // Function call to initialize app
 // init();
