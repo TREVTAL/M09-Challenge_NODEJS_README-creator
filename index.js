3e6652ad65543392db6c8a77d1cf49c4d473b17d
TREVTAL <trevtal@gmail.com>
2022-08-14 00:11:57 -0500

    added files to gitignore, setup package json and started questions array

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the Projetc's title?" ,
+    "Can you briefly describe your project (please include why you did it and what problem you're solving)?" ,
+    "Please fill out starting with step number and separating steps with semicolon (;)\n What are the installation instructions?" ,
+    "Please fill out starting with step number and separating steps with semicolon (;)\n What are the usage instructions?" ,
+    "Please fill out starting with step number and separating steps with semicolon (;)\n Who do you want to give credit to" ,
+    "Please fill out starting with step number and separating steps with semicolon (;)\n What are the test instructions?" ,
+    "Please select the type of license your working with",
+    "Please enter your GitHub User",
+    "Please enter you email address"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
